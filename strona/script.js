be4c658ab5c4fd3df6bba5d2b56e3dbf625d13df
stronabezsensu <antekhawro@gmail.com>
2025-11-06 17:13:51 +0100

    Delete strona directory

--- strona/script.js
+++
@@ -1,8 +0,0 @@
-const guziczek = document.getElementById("guziczek");
-const wynik = document.getElementById("wynik");
-
-var a = 1;
-guziczek.addEventListener("click", () => {
-    wynik.innerText = "wynik: " + a++
-    // wynik.innerText = `wynik: ${a}`
-})
